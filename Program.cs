27ae24daca38ecc5b73fe836abf368f8ab17ba12
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-04-01 12:25:04 +0530

    Ganesh Adding Greeting Message

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ProductReviewManagementWithLinq
+{
+    class Program
+    {
+
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the project review management");
+            List<ProductReview> list = new List<ProductReview>();
+        }
+     }
+        
+}   
